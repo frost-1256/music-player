6ddf493028d9eeb7c1369b4c4daaa1ba1b1cfb5d
spring <harusan@spring-server.com>
2026-04-15 17:03:45 +0900

    C#なんもわからん部

--- music-player/Program.cs
+++ music-player/Program.cs
@@ -1,2 +1,18 @@
 ﻿// C#なんもわからん部
-Console.WriteLine("aaaa");
+using Terminal.Gui.App;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
+
+using IApplication app = Application.Create ();
+app.Init ();
+
+using Window window = new () { Title = "Hello World (Esc to quit)" };
+Label label = new ()
+{
+    Text = "Hello, Terminal.Gui v2!",
+    X = Pos.Center (),
+    Y = Pos.Center ()
+};
+window.Add (label);
+
+app.Run (window);
